a75738e0bfc8a7367c6793c064fb033956611994
Paul Stevens <ps@nosignal.io>
2019-11-22 18:09:15 +0000

    hacking around various limitations

--- backend.tf
+++ backend.tf
@@ -1,14 +1,7 @@
 terraform {
-    backend "s3" {}
-}
-
-data "terraform_remote_state" "state" {
-    backend = "s3"
-    config {
+    backend "s3" {
         bucket      = "opshell"
         key         = "terraform.tfstate"
-        access_key  = "${var.DO_SPACES_KEY}"
-        secret_key  = "${var.DO_SECRET_KEY}"
         endpoint    = "https://fra1.digitaloceanspaces.com"
         region      = "eu-central-1"
 
