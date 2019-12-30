1944201d192748ae893f71f6da5e9f68da340cc0
Paul Stevens <ps@nosignal.io>
2019-12-30 16:12:54 +0200

    test new pipelines

---
+++ bin/digitalocean/deployment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errno
+set -o nounset
+
+env=$1
+
+if [ "$#" -ne 1 ]; then
+    echo "No environment specified."
+    exit 1
+fi
+
+# environments to an array
+envs=(common $env)
+
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+for e in ${envs[@]}; do
+    kubectl --kubeconfig=/dev/null --server=$KUBERNETES_SERVER --certificate-authority=cert.crt --token=$KUBERNETES_TOKEN apply -f kubernetes/digitalocean/${e}/
+done
---
+++ test/opshell/runner.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -o errno
+set -o nounset
+
+exit 0
