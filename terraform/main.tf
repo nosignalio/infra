e28db61c9802515d2b07e95167abb82cb6bfeffc
Paul Stevens <ps@nosignal.io>
2019-12-30 11:11:35 +0200

    spin up kubernetes cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 provider "digitalocean" {}
 
+data "digitalocean_domain" "opshell" {
+  name      = "opshell.io"
+}
+
+# primary shell server
 resource "digitalocean_droplet" "cnc" {
   image     = "ubuntu-18-04-x64"
   name      = "cnc.opshell.io"
@@ -10,14 +15,23 @@ resource "digitalocean_droplet" "cnc" {
   private_networking  = true
 }
 
-data "digitalocean_domain" "opshell" {
-  name      = "opshell.io"
-}
-
 resource "digitalocean_record" "cnc" {
   domain    = data.digitalocean_domain.opshell.name
   type      = "A"
   name      = "cnc"
   value     = digitalocean_droplet.cnc.ipv4_address
   ttl       = "300"
+}
+
+# kubernetes cluster
+resource "digitalocean_kubernetes_cluster" "opshell" {
+  name      = "opshell"
+  region    = "lon1"
+  version   = "1.16.2-do.1"
+
+  node_pool {
+    name        = "worker-pool"
+    size        = "s-2vcpu-4gb"
+    node_count  = "3"
+  }
 }
